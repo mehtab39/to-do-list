a8614f7f03197b697c1f87ce1d51a884d1e72fdc
GILL <msgill8146878895@gmail.com>
2022-03-15 12:37:44 +0530

    adding references

--- src/component/Form/DatePicker.jsx
+++ src/component/Form/DatePicker.jsx
@@ -1,3 +1,4 @@
+//reference: https://mui.com/components/date-time-picker/
 import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
@@ -5,13 +6,12 @@ import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DateTimePicker from '@mui/lab/DateTimePicker';
 
 export default function DatePicker({date, setDate}) {
- 
-
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
       <DateTimePicker
         renderInput={(props) => <TextField {...props} />}
-        label="Set Time"
+        label="Set Deadline"
+        minWidth="700px"
         minDate={new Date()}
         value={date}
         onChange={(newValue) => {
--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -4,7 +4,7 @@ import { AppBar,  Toolbar, Typography } from "@mui/material"
 export const Header = $ => {
   return <><AppBar position="absolute">
     <Toolbar>
-      <Typography component="h1" variant="h6" color="inherit" nowrapr="true">
+      <Typography component="h1" variant="h6" color="secondary" nowrapr="true">
           TO-DO TO MAKE YOUR LIFE SIMPLER
       </Typography>
   </Toolbar>
